7a04a145603d9d4b1e1d47c1010698f1531a322a
DeckerAl <bellotunde87@gmail.com>
2022-10-13 23:26:22 -0700

    six finally

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	printf("Size of integer: %i bytes\n", sizeof(int));
 	printf("Size of float: %i bytes\n", sizeof(float));
 	printf("Size of long int: %i bytes\n", sizeof(long int));
-	printf("SIze of long long int %ibytes\n",sizeof(long long int));
+	printf("SIze of long long int: %ibytes\n", sizeof(long long int));
 	printf("Size of char: %i bytes\n", sizeof(char));
 
 	return (0);
