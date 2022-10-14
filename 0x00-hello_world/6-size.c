841f02459a46a20e04428df6763e273a0a7893c8
DeckerAl <bellotunde87@gmail.com>
2022-10-13 23:49:59 -0700

    solved 6!

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("SIze of a long long int: %ibyte(s)\n", sizeof(long long int));
+	printf("SIze of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 
 	return (0);
