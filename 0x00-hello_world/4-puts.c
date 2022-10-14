eee407c0e2e6462e9fb3f2208c5b1e85854830cf
DeckerAl <bellotunde87@gmail.com>
2022-10-13 22:43:28 -0700

    Finishing touches

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 	puts("\"Programming is like building a mutlilingual puzzle");
 	return(0);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 	int integerSize;
 	float floatSize;
 	double doubleSize;
 	char charSize;
 
-	printf("Size of integer: %i bytes\n", sizeof(integerSize));
-	printf("Size of float: %i bytes\n", sizeof(floatSize));
-	printf("Size of double: %i bytes\n", sizeof(doubleSize));
-	printf("Size of char: %i bytes\n", sizeof(charSize));
+	printf("Size of integer: %li bytes\n", sizeof(integerSize));
+	printf("Size of float: %li bytes\n", sizeof(floatSize));
+	printf("Size of double: %li bytes\n", sizeof(doubleSize));
+	printf("Size of char: %li bytes\n", sizeof(charSize));
 
 	return(0)
 }
