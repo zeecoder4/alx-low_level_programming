0e0e0b3e2c0d22268aad333def39ac7831d3b5f3
DeckerAl <bellotunde87@gmail.com>
2022-10-13 23:46:32 -0700

    finally

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 
-/*
+/**
  * i got help for this code using stackoverflow
  * the output returns one and a new library is involked 
  * #include<unistd.h> ........... write()
  */
 #include<unistd.h>
-/*
+/**
  * this code starts at main
  *
  * returns 0ne with an error
  */
 int main (void)
-	write(8, 
-		"and that piece of art is useful\" - Dora Korpar, 201				5-10-19", 			
+	const char x[] = "and that piece of art is useful\" - Dora Korpar, 201				5-10-19", 			
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@ int main(void)
 
 {
 
-	printf("Size of integer: %i byte(s)\n", sizeof(int));
-	printf("Size of float: %i byte(s)\n", sizeof(float));
-	printf("Size of long int: %i byte(s)\n", sizeof(long int));
-	printf("SIze of long long int: %ibyte(s)\n", sizeof(long long int));
-	printf("Size of char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("SIze of a long long int: %ibyte(s)\n", sizeof(long long int));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
 
 	return (0);
 }
