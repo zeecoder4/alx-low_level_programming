0760f1e0ea43c08b7e2e7617a9d71fc5f311e208
DeckerAl <bellotunde87@gmail.com>
2023-01-01 12:11:52 -0800

    Started task 3

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * main - test function that prints If  integer is postivie or negative
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	postive_or_negative(i);
+
+	return(0);
+}
